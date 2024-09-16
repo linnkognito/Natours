26b296fe55c854380240c58840051b75857875d3
Linn Johansen <linn.es.johansen@gmail.com>
2024-09-16 14:30:38 -0700

    Added SIGTERM handling

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,11 +20,12 @@ const createAndSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    httpOnly: true, //
+    httpOnly: true,
   };
 
   // Send cookie
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // only set secure prop for production mode
+
   res.cookie('jwt', token, cookieOptions);
 
   // Remove password from the output:
